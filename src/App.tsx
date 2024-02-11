8a15b6c7a61647b6aa2d68f71d1eeb404f7fdb21
Francis Beam Santander <santanderfrancisbeam@gmail.com>
2024-02-11 17:04:50 +0800

    wip customer and admin routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/Footer";
-import About from "./pages/About";
-
+import Routes from "../src/routes/index.tsx";
+import AuthProvider from "./app/AuthProvider.tsx";
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </>
   );
 };
